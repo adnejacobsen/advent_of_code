86d2e9193b8cdd64699fc4cd7e4a0c5f7a032f1e
Ådne Jacobsen <adnj@equinor.com>
2022-12-06 06:38:14 +0100

    day6

---
+++ 2022/src/day6/index.js
@@ -0,0 +1,38 @@
+import { Day } from "../utils.js";
+
+class Day6 extends Day {
+    constructor() {
+        super(6);
+    }
+
+    preprocess(text) {
+        return text.split("");
+    }
+
+    #findMarker(input, length) {
+        let index = 0;
+        let done = false;
+
+        while (!done) {
+            let chars = input.slice(index, index + length);
+
+            done = chars.every((c) => {
+                return chars.filter((c2) => c2 === c).length === 1;
+            });
+
+            index++;
+        }
+
+        return index + (length - 1);
+    }
+
+    partOne() {
+        return this.#findMarker(this.input, 4);
+    }
+
+    partTwo() {
+        return this.#findMarker(this.input, 14);
+    }
+}
+
+new Day6().solve();
